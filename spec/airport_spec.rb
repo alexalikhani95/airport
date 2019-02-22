f7625cbccbce34923757cfab9a869dcab5e61ef5
Alex <alikhanialex@gmail.com>
2019-02-22 20:51:29 +0000

    first test written for landing a plane

---
+++ spec/airport_spec.rb
@@ -0,0 +1,10 @@
+require 'ariport'
+require 'airplane'
+
+describe Airport do
+  it 'is able to land an airplane' do
+    gatwick = Airport.new
+    airbus = Airplane.new
+    expect(gatwick.land(airbus)).to eq [airbus]
+  end
+end
