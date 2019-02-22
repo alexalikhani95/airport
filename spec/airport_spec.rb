8b2132247622da9ad7767612cf184c4cdd9f72db
Alex <alikhanialex@gmail.com>
2019-02-22 22:03:44 +0000

    test written and passing to ensure that taken off plane is no longer in the airport

--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -24,4 +24,14 @@ describe Airport do
     gatwick.land(concorde)
     expect(gatwick.takeoff(airbus)).to eq airbus
   end
+
+  it 'makes sure that the taken off airplane is no longer in the airport' do
+    gatwick = Airport.new
+    airbus = Airplane.new
+    concorde = Airplane.new
+    gatwick.land(airbus)
+    gatwick.land(concorde)
+    gatwick.takeoff(concorde)
+    expect(gatwick.planes).to eq [airbus]
+  end
 end
